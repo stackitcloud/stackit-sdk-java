5d4d5ff165853c27cd4a2504bae59f82f72b76ee
Alexander Dahmen <alexander.dahmen@inovex.de>
2025-09-01 10:49:31 +0200

    feature(core): Add wait handler structure
    Signed-off-by: Alexander Dahmen <alexander.dahmen@inovex.de>

---
+++ core/src/main/java/cloud/stackit/sdk/core/oapierror/GenericOpenAPIError.java
@@ -0,0 +1,75 @@
+package cloud.stackit.sdk.core.oapierror;
+
+import cloud.stackit.sdk.core.exception.ApiException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+public class GenericOpenAPIError extends ApiException {
+
+	// When a response has a bad status, this limits the number of characters that are shown from
+	// the response Body
+	public static int ApiErrorMaxCharacterLimit = 500;
+
+	private int statusCode;
+	private byte[] body;
+	private String errorMessage;
+	private Object model;
+
+	public GenericOpenAPIError(ApiException e) {
+		this.statusCode = e.getCode();
+		this.errorMessage = e.getMessage();
+	}
+
+	public GenericOpenAPIError(int statusCode, String errorMessage) {
+		this(statusCode, errorMessage, null, new HashMap<>());
+	}
+
+	public GenericOpenAPIError(int statusCode, String errorMessage, byte[] body, Object model) {
+		this.statusCode = statusCode;
+		this.errorMessage = errorMessage;
+		this.body = body;
+		this.model = model;
+	}
+
+	@Override
+	public String getMessage() {
+		// Prevent negative values
+		if (ApiErrorMaxCharacterLimit < 0) {
+			ApiErrorMaxCharacterLimit = 500;
+		}
+
+		if (body == null) {
+			return String.format("%s, status code %d", errorMessage, statusCode);
+		}
+
+		String bodyStr = new String(body, StandardCharsets.UTF_8);
+
+		if (bodyStr.length() <= ApiErrorMaxCharacterLimit) {
+			return String.format("%s, status code %d, Body: %s", errorMessage, statusCode, bodyStr);
+		}
+
+		int indexStart = ApiErrorMaxCharacterLimit / 2;
+		int indexEnd = bodyStr.length() - ApiErrorMaxCharacterLimit / 2;
+		int numberTruncatedCharacters = indexEnd - indexStart;
+
+		return String.format(
+				"%s, status code %d, Body: %s [...truncated %d characters...] %s",
+				errorMessage,
+				statusCode,
+				bodyStr.substring(0, indexStart),
+				numberTruncatedCharacters,
+				bodyStr.substring(indexEnd));
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public byte[] getBody() {
+		return body;
+	}
+
+	public Object getModel() {
+		return model;
+	}
+}
--- examples/resourcemanager/src/main/java/cloud/stackit/sdk/resourcemanager/examples/ResourcemanagerExample.java
+++ examples/resourcemanager/src/main/java/cloud/stackit/sdk/resourcemanager/examples/ResourcemanagerExample.java
@@ -12,13 +12,13 @@ import cloud.stackit.sdk.resourcemanager.model.OrganizationResponse;
 import cloud.stackit.sdk.resourcemanager.model.PartialUpdateFolderPayload;
 import cloud.stackit.sdk.resourcemanager.model.PartialUpdateProjectPayload;
 import cloud.stackit.sdk.resourcemanager.model.Project;
-import java.io.IOException;
+import cloud.stackit.sdk.resourcemanager.wait.ResourcemanagerWait;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.UUID;
 
 class ResourcemanagerExample {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		// Credentials are read from the credentialsFile in `~/.stackit/credentials.json` or the env
 		// STACKIT_SERVICE_ACCOUNT_KEY_PATH / STACKIT_SERVICE_ACCOUNT_KEY
 		ResourceManagerApi resourceManagerApi = new ResourceManagerApi();
@@ -64,8 +64,13 @@ class ResourcemanagerExample {
 							new CreateFolderPayload()
 									.containerParentId(containerParentId.toString())
 									.name("java-testing-folder")
+									.addMembersItem(member)
 									.labels(Collections.singletonMap("foo", "bar")));
 			System.out.println("Folder: \n" + folder.toString());
+
+			ResourcemanagerWait.createProjectWaitHandler(
+							resourceManagerApi, project.getContainerId())
+					.waitWithContext();
 
 			/* list folders */
 			ListFoldersResponse responseListFolders =
@@ -90,6 +95,10 @@ class ResourcemanagerExample {
 					project.getContainerId(),
 					new PartialUpdateProjectPayload().containerParentId(folder.getContainerId()));
 
+			ResourcemanagerWait.createProjectWaitHandler(
+							resourceManagerApi, project.getContainerId())
+					.waitWithContext();
+
 			/* get organization details */
 			OrganizationResponse organizationResponse =
 					resourceManagerApi.getOrganization(organizationIdString);
@@ -102,6 +111,10 @@ class ResourcemanagerExample {
 
 			/* delete project */
 			resourceManagerApi.deleteProject(project.getContainerId());
+
+			ResourcemanagerWait.deleteProjectWaitHandler(
+							resourceManagerApi, project.getContainerId())
+					.waitWithContext();
 
 			/* delete folder */
 			resourceManagerApi.deleteFolder(folder.getContainerId(), true);
