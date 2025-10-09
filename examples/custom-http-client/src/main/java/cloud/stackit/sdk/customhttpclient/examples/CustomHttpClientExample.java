c6b1de5e2c013a99aaa7984e69596134fe2d2c89
Benjosh95 <benjamin.fuertsch@inovex.de>
2025-10-09 19:22:05 +0200

    fix pmd commentsize

--- examples/custom-http-client/src/main/java/cloud/stackit/sdk/customhttpclient/examples/CustomHttpClientExample.java
+++ examples/custom-http-client/src/main/java/cloud/stackit/sdk/customhttpclient/examples/CustomHttpClientExample.java
@@ -14,10 +14,11 @@ import okhttp3.OkHttpClient;
  *
  * The example shows how to set the authorization header in the OkHttpClient object (required!).
  *
- * NOTE: Passing the http client is optional, see our other examples where no OkHttpClient object is passed.
+ * NOTE: Passing the http client is optional, see our other examples
+ * where no OkHttpClient object is passed.
  * In this case the STACKIT SDK ApiClients will just create their own OkHttpClient objects.
- * Nevertheless, for production usage try to use one single OkHttpClient object for everything to take advantage of the
- * shared connection pool and to prevent resource leaks.
+ * Nevertheless, for production usage try to use one single OkHttpClient object 
+ * for everything to take advantage of the shared connection pool and to prevent resource leaks.
  *
  * */
 public class CustomHttpClientExample {
