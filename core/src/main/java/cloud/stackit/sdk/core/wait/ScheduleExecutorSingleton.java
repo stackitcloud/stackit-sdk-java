d2a1a5553baa3c177651cf5f4dc910ac1accaafc
Alexander Dahmen <alexander.dahmen@inovex.de>
2025-10-24 13:56:12 +0200

    chore(core): Make SchedulerExecutor thread pool size configurable
    Signed-off-by: Alexander Dahmen <alexander.dahmen@inovex.de>

--- core/src/main/java/cloud/stackit/sdk/core/wait/ScheduleExecutorSingleton.java
+++ core/src/main/java/cloud/stackit/sdk/core/wait/ScheduleExecutorSingleton.java
@@ -3,14 +3,32 @@ package cloud.stackit.sdk.core.wait;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-@SuppressWarnings("PMD.DoNotUseThreads")
+@SuppressWarnings({
+	"PMD.DoNotUseThreads",
+	"PMD.SingleMethodSingleton",
+	"PMD.NonThreadSafeSingleton"
+})
 public final class ScheduleExecutorSingleton {
-	// Pool size for the thread pool
-	private static final int POOL_SIZE = 1;
+	// Default pool size for the thread pool
+	private static final int DEFAULT_POOL_SIZE = 1;
+	private static ScheduleExecutorSingleton instance;
+	private static final Lock LOCK = new ReentrantLock();
 	private final ScheduledExecutorService scheduler;
 
+	/** Default constructor which takes the default pool size */
 	private ScheduleExecutorSingleton() {
+		this(DEFAULT_POOL_SIZE);
+	}
+
+	/**
+	 * Constructor to set a different pool size
+	 *
+	 * @param poolSize
+	 */
+	private ScheduleExecutorSingleton(int poolSize) {
 		// Use Daemon threads to prevent that the user need to call shutdown
 		// even if its program was already terminated
 		ThreadFactory daemonThreadFactory =
@@ -19,20 +37,28 @@ public final class ScheduleExecutorSingleton {
 					thread.setDaemon(true);
 					return thread;
 				};
-		scheduler = Executors.newScheduledThreadPool(POOL_SIZE, daemonThreadFactory);
+		this.scheduler = Executors.newScheduledThreadPool(poolSize, daemonThreadFactory);
+	}
+
+	public static ScheduleExecutorSingleton getInstance() {
+		return getInstance(DEFAULT_POOL_SIZE);
+	}
+
+	public static ScheduleExecutorSingleton getInstance(int poolSize) {
+		if (instance == null) {
+			LOCK.lock();
+			try {
+				if (instance == null) {
+					instance = new ScheduleExecutorSingleton(poolSize);
+				}
+			} finally {
+				LOCK.unlock();
+			}
+		}
+		return instance;
 	}
 
 	public ScheduledExecutorService getScheduler() {
 		return scheduler;
-	}
-
-	// In order to make the Linter happy this is the only solution which is accepted.
-	// Lock/ReentrantLock, synchronized and volatile are in general not accepted.
-	private static final class SingletonHolder {
-		public static final ScheduleExecutorSingleton INSTANCE = new ScheduleExecutorSingleton();
-	}
-
-	public static ScheduleExecutorSingleton getInstance() {
-		return SingletonHolder.INSTANCE;
 	}
 }
