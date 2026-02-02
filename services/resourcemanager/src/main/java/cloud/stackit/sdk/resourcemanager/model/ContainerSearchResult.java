e83b74fd4750b51dcd155859ca92eec35a279723
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2026-02-02 11:11:34 +0100

    Generator: Update SDK /services/resourcemanager (#273)

--- services/resourcemanager/src/main/java/cloud/stackit/sdk/resourcemanager/model/ContainerSearchResult.java
+++ services/resourcemanager/src/main/java/cloud/stackit/sdk/resourcemanager/model/ContainerSearchResult.java
@@ -25,10 +25,11 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.UUID;
 
 /** ContainerSearchResult */
@@ -239,6 +240,50 @@ public class ContainerSearchResult {
 		this.organizationId = organizationId;
 	}
 
+	/**
+	 * A container for additional, undeclared properties. This is a holder for any undeclared
+	 * properties as specified with the 'additionalProperties' keyword in the OAS document.
+	 */
+	private Map<String, Object> additionalProperties;
+
+	/**
+	 * Set the additional (undeclared) property with the specified name and value. If the property
+	 * does not already exist, create it otherwise replace it.
+	 *
+	 * @param key name of the property
+	 * @param value value of the property
+	 * @return the ContainerSearchResult instance itself
+	 */
+	public ContainerSearchResult putAdditionalProperty(String key, Object value) {
+		if (this.additionalProperties == null) {
+			this.additionalProperties = new HashMap<String, Object>();
+		}
+		this.additionalProperties.put(key, value);
+		return this;
+	}
+
+	/**
+	 * Return the additional (undeclared) property.
+	 *
+	 * @return a map of objects
+	 */
+	public Map<String, Object> getAdditionalProperties() {
+		return additionalProperties;
+	}
+
+	/**
+	 * Return the additional (undeclared) property with the specified name.
+	 *
+	 * @param key name of the property
+	 * @return an object
+	 */
+	public Object getAdditionalProperty(String key) {
+		if (this.additionalProperties == null) {
+			return null;
+		}
+		return this.additionalProperties.get(key);
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) {
@@ -253,12 +298,21 @@ public class ContainerSearchResult {
 				&& Objects.equals(this.id, containerSearchResult.id)
 				&& Objects.equals(this.lifecycleState, containerSearchResult.lifecycleState)
 				&& Objects.equals(this.name, containerSearchResult.name)
-				&& Objects.equals(this.organizationId, containerSearchResult.organizationId);
+				&& Objects.equals(this.organizationId, containerSearchResult.organizationId)
+				&& Objects.equals(
+						this.additionalProperties, containerSearchResult.additionalProperties);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(containerId, containerType, id, lifecycleState, name, organizationId);
+		return Objects.hash(
+				containerId,
+				containerType,
+				id,
+				lifecycleState,
+				name,
+				organizationId,
+				additionalProperties);
 	}
 
 	@Override
@@ -271,6 +325,9 @@ public class ContainerSearchResult {
 		sb.append("    lifecycleState: ").append(toIndentedString(lifecycleState)).append("\n");
 		sb.append("    name: ").append(toIndentedString(name)).append("\n");
 		sb.append("    organizationId: ").append(toIndentedString(organizationId)).append("\n");
+		sb.append("    additionalProperties: ")
+				.append(toIndentedString(additionalProperties))
+				.append("\n");
 		sb.append("}");
 		return sb.toString();
 	}
@@ -320,17 +377,6 @@ public class ContainerSearchResult {
 						String.format(
 								"The required field(s) %s in ContainerSearchResult is not found in the empty JSON string",
 								ContainerSearchResult.openapiRequiredFields.toString()));
-			}
-		}
-
-		Set<Map.Entry<String, JsonElement>> entries = jsonElement.getAsJsonObject().entrySet();
-		// check to see if the JSON string contains additional fields
-		for (Map.Entry<String, JsonElement> entry : entries) {
-			if (!ContainerSearchResult.openapiFields.contains(entry.getKey())) {
-				throw new IllegalArgumentException(
-						String.format(
-								"The field `%s` in the JSON string is not defined in the `ContainerSearchResult` properties. JSON: %s",
-								entry.getKey(), jsonElement.toString()));
 			}
 		}
 
@@ -400,6 +446,30 @@ public class ContainerSearchResult {
 						public void write(JsonWriter out, ContainerSearchResult value)
 								throws IOException {
 							JsonObject obj = thisAdapter.toJsonTree(value).getAsJsonObject();
+							obj.remove("additionalProperties");
+							// serialize additional properties
+							if (value.getAdditionalProperties() != null) {
+								for (Map.Entry<String, Object> entry :
+										value.getAdditionalProperties().entrySet()) {
+									if (entry.getValue() instanceof String)
+										obj.addProperty(entry.getKey(), (String) entry.getValue());
+									else if (entry.getValue() instanceof Number)
+										obj.addProperty(entry.getKey(), (Number) entry.getValue());
+									else if (entry.getValue() instanceof Boolean)
+										obj.addProperty(entry.getKey(), (Boolean) entry.getValue());
+									else if (entry.getValue() instanceof Character)
+										obj.addProperty(
+												entry.getKey(), (Character) entry.getValue());
+									else {
+										JsonElement jsonElement = gson.toJsonTree(entry.getValue());
+										if (jsonElement.isJsonArray()) {
+											obj.add(entry.getKey(), jsonElement.getAsJsonArray());
+										} else {
+											obj.add(entry.getKey(), jsonElement.getAsJsonObject());
+										}
+									}
+								}
+							}
 							elementAdapter.write(out, obj);
 						}
 
@@ -407,7 +477,40 @@ public class ContainerSearchResult {
 						public ContainerSearchResult read(JsonReader in) throws IOException {
 							JsonElement jsonElement = elementAdapter.read(in);
 							validateJsonElement(jsonElement);
-							return thisAdapter.fromJsonTree(jsonElement);
+							JsonObject jsonObj = jsonElement.getAsJsonObject();
+							// store additional fields in the deserialized instance
+							ContainerSearchResult instance = thisAdapter.fromJsonTree(jsonObj);
+							for (Map.Entry<String, JsonElement> entry : jsonObj.entrySet()) {
+								if (!openapiFields.contains(entry.getKey())) {
+									if (entry.getValue().isJsonPrimitive()) { // primitive type
+										if (entry.getValue().getAsJsonPrimitive().isString())
+											instance.putAdditionalProperty(
+													entry.getKey(), entry.getValue().getAsString());
+										else if (entry.getValue().getAsJsonPrimitive().isNumber())
+											instance.putAdditionalProperty(
+													entry.getKey(), entry.getValue().getAsNumber());
+										else if (entry.getValue().getAsJsonPrimitive().isBoolean())
+											instance.putAdditionalProperty(
+													entry.getKey(),
+													entry.getValue().getAsBoolean());
+										else
+											throw new IllegalArgumentException(
+													String.format(
+															"The field `%s` has unknown primitive type. Value: %s",
+															entry.getKey(),
+															entry.getValue().toString()));
+									} else if (entry.getValue().isJsonArray()) {
+										instance.putAdditionalProperty(
+												entry.getKey(),
+												gson.fromJson(entry.getValue(), List.class));
+									} else { // JSON object
+										instance.putAdditionalProperty(
+												entry.getKey(),
+												gson.fromJson(entry.getValue(), HashMap.class));
+									}
+								}
+							}
+							return instance;
 						}
 					}.nullSafe();
 		}
--- services/resourcemanager/src/main/java/cloud/stackit/sdk/resourcemanager/model/ListFoldersResponseItemsInner.java
+++ services/resourcemanager/src/main/java/cloud/stackit/sdk/resourcemanager/model/ListFoldersResponseItemsInner.java
@@ -27,9 +27,9 @@ import java.time.OffsetDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.UUID;
 
 /** ListFoldersResponseItemsInner */
@@ -226,6 +226,50 @@ public class ListFoldersResponseItemsInner {
 		this.updateTime = updateTime;
 	}
 
+	/**
+	 * A container for additional, undeclared properties. This is a holder for any undeclared
+	 * properties as specified with the 'additionalProperties' keyword in the OAS document.
+	 */
+	private Map<String, Object> additionalProperties;
+
+	/**
+	 * Set the additional (undeclared) property with the specified name and value. If the property
+	 * does not already exist, create it otherwise replace it.
+	 *
+	 * @param key name of the property
+	 * @param value value of the property
+	 * @return the ListFoldersResponseItemsInner instance itself
+	 */
+	public ListFoldersResponseItemsInner putAdditionalProperty(String key, Object value) {
+		if (this.additionalProperties == null) {
+			this.additionalProperties = new HashMap<String, Object>();
+		}
+		this.additionalProperties.put(key, value);
+		return this;
+	}
+
+	/**
+	 * Return the additional (undeclared) property.
+	 *
+	 * @return a map of objects
+	 */
+	public Map<String, Object> getAdditionalProperties() {
+		return additionalProperties;
+	}
+
+	/**
+	 * Return the additional (undeclared) property with the specified name.
+	 *
+	 * @param key name of the property
+	 * @return an object
+	 */
+	public Object getAdditionalProperty(String key) {
+		if (this.additionalProperties == null) {
+			return null;
+		}
+		return this.additionalProperties.get(key);
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) {
@@ -242,12 +286,23 @@ public class ListFoldersResponseItemsInner {
 				&& Objects.equals(this.labels, listFoldersResponseItemsInner.labels)
 				&& Objects.equals(this.name, listFoldersResponseItemsInner.name)
 				&& Objects.equals(this.parent, listFoldersResponseItemsInner.parent)
-				&& Objects.equals(this.updateTime, listFoldersResponseItemsInner.updateTime);
+				&& Objects.equals(this.updateTime, listFoldersResponseItemsInner.updateTime)
+				&& Objects.equals(
+						this.additionalProperties,
+						listFoldersResponseItemsInner.additionalProperties);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(containerId, creationTime, folderId, labels, name, parent, updateTime);
+		return Objects.hash(
+				containerId,
+				creationTime,
+				folderId,
+				labels,
+				name,
+				parent,
+				updateTime,
+				additionalProperties);
 	}
 
 	@Override
@@ -261,6 +316,9 @@ public class ListFoldersResponseItemsInner {
 		sb.append("    name: ").append(toIndentedString(name)).append("\n");
 		sb.append("    parent: ").append(toIndentedString(parent)).append("\n");
 		sb.append("    updateTime: ").append(toIndentedString(updateTime)).append("\n");
+		sb.append("    additionalProperties: ")
+				.append(toIndentedString(additionalProperties))
+				.append("\n");
 		sb.append("}");
 		return sb.toString();
 	}
@@ -322,17 +380,6 @@ public class ListFoldersResponseItemsInner {
 			}
 		}
 
-		Set<Map.Entry<String, JsonElement>> entries = jsonElement.getAsJsonObject().entrySet();
-		// check to see if the JSON string contains additional fields
-		for (Map.Entry<String, JsonElement> entry : entries) {
-			if (!ListFoldersResponseItemsInner.openapiFields.contains(entry.getKey())) {
-				throw new IllegalArgumentException(
-						String.format(
-								"The field `%s` in the JSON string is not defined in the `ListFoldersResponseItemsInner` properties. JSON: %s",
-								entry.getKey(), jsonElement.toString()));
-			}
-		}
-
 		// check to make sure all required properties/fields are present in the JSON string
 		for (String requiredField : ListFoldersResponseItemsInner.openapiRequiredFields) {
 			if (jsonElement.getAsJsonObject().get(requiredField) == null) {
@@ -384,6 +431,30 @@ public class ListFoldersResponseItemsInner {
 						public void write(JsonWriter out, ListFoldersResponseItemsInner value)
 								throws IOException {
 							JsonObject obj = thisAdapter.toJsonTree(value).getAsJsonObject();
+							obj.remove("additionalProperties");
+							// serialize additional properties
+							if (value.getAdditionalProperties() != null) {
+								for (Map.Entry<String, Object> entry :
+										value.getAdditionalProperties().entrySet()) {
+									if (entry.getValue() instanceof String)
+										obj.addProperty(entry.getKey(), (String) entry.getValue());
+									else if (entry.getValue() instanceof Number)
+										obj.addProperty(entry.getKey(), (Number) entry.getValue());
+									else if (entry.getValue() instanceof Boolean)
+										obj.addProperty(entry.getKey(), (Boolean) entry.getValue());
+									else if (entry.getValue() instanceof Character)
+										obj.addProperty(
+												entry.getKey(), (Character) entry.getValue());
+									else {
+										JsonElement jsonElement = gson.toJsonTree(entry.getValue());
+										if (jsonElement.isJsonArray()) {
+											obj.add(entry.getKey(), jsonElement.getAsJsonArray());
+										} else {
+											obj.add(entry.getKey(), jsonElement.getAsJsonObject());
+										}
+									}
+								}
+							}
 							elementAdapter.write(out, obj);
 						}
 
@@ -392,7 +463,41 @@ public class ListFoldersResponseItemsInner {
 								throws IOException {
 							JsonElement jsonElement = elementAdapter.read(in);
 							validateJsonElement(jsonElement);
-							return thisAdapter.fromJsonTree(jsonElement);
+							JsonObject jsonObj = jsonElement.getAsJsonObject();
+							// store additional fields in the deserialized instance
+							ListFoldersResponseItemsInner instance =
+									thisAdapter.fromJsonTree(jsonObj);
+							for (Map.Entry<String, JsonElement> entry : jsonObj.entrySet()) {
+								if (!openapiFields.contains(entry.getKey())) {
+									if (entry.getValue().isJsonPrimitive()) { // primitive type
+										if (entry.getValue().getAsJsonPrimitive().isString())
+											instance.putAdditionalProperty(
+													entry.getKey(), entry.getValue().getAsString());
+										else if (entry.getValue().getAsJsonPrimitive().isNumber())
+											instance.putAdditionalProperty(
+													entry.getKey(), entry.getValue().getAsNumber());
+										else if (entry.getValue().getAsJsonPrimitive().isBoolean())
+											instance.putAdditionalProperty(
+													entry.getKey(),
+													entry.getValue().getAsBoolean());
+										else
+											throw new IllegalArgumentException(
+													String.format(
+															"The field `%s` has unknown primitive type. Value: %s",
+															entry.getKey(),
+															entry.getValue().toString()));
+									} else if (entry.getValue().isJsonArray()) {
+										instance.putAdditionalProperty(
+												entry.getKey(),
+												gson.fromJson(entry.getValue(), List.class));
+									} else { // JSON object
+										instance.putAdditionalProperty(
+												entry.getKey(),
+												gson.fromJson(entry.getValue(), HashMap.class));
+									}
+								}
+							}
+							return instance;
 						}
 					}.nullSafe();
 		}
