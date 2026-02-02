a4f4534d48aeb1ea564f57e303711b315448fdd7
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2026-02-02 11:45:51 +0100

    Generator: Update SDK /services/loadbalancer (#271)

--- services/loadbalancer/src/main/java/cloud/stackit/sdk/loadbalancer/model/OptionsTCP.java
+++ services/loadbalancer/src/main/java/cloud/stackit/sdk/loadbalancer/model/OptionsTCP.java
@@ -24,10 +24,11 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 
 /**
  * ProtocolOptionsTCP options to be configured for the PROTOCOL_TCP, PROTOCOL_TCP_PROXY, and
@@ -64,6 +65,50 @@ public class OptionsTCP {
 		this.idleTimeout = idleTimeout;
 	}
 
+	/**
+	 * A container for additional, undeclared properties. This is a holder for any undeclared
+	 * properties as specified with the 'additionalProperties' keyword in the OAS document.
+	 */
+	private Map<String, Object> additionalProperties;
+
+	/**
+	 * Set the additional (undeclared) property with the specified name and value. If the property
+	 * does not already exist, create it otherwise replace it.
+	 *
+	 * @param key name of the property
+	 * @param value value of the property
+	 * @return the OptionsTCP instance itself
+	 */
+	public OptionsTCP putAdditionalProperty(String key, Object value) {
+		if (this.additionalProperties == null) {
+			this.additionalProperties = new HashMap<String, Object>();
+		}
+		this.additionalProperties.put(key, value);
+		return this;
+	}
+
+	/**
+	 * Return the additional (undeclared) property.
+	 *
+	 * @return a map of objects
+	 */
+	public Map<String, Object> getAdditionalProperties() {
+		return additionalProperties;
+	}
+
+	/**
+	 * Return the additional (undeclared) property with the specified name.
+	 *
+	 * @param key name of the property
+	 * @return an object
+	 */
+	public Object getAdditionalProperty(String key) {
+		if (this.additionalProperties == null) {
+			return null;
+		}
+		return this.additionalProperties.get(key);
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) {
@@ -73,12 +118,13 @@ public class OptionsTCP {
 			return false;
 		}
 		OptionsTCP optionsTCP = (OptionsTCP) o;
-		return Objects.equals(this.idleTimeout, optionsTCP.idleTimeout);
+		return Objects.equals(this.idleTimeout, optionsTCP.idleTimeout)
+				&& Objects.equals(this.additionalProperties, optionsTCP.additionalProperties);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(idleTimeout);
+		return Objects.hash(idleTimeout, additionalProperties);
 	}
 
 	@Override
@@ -86,6 +132,9 @@ public class OptionsTCP {
 		StringBuilder sb = new StringBuilder();
 		sb.append("class OptionsTCP {\n");
 		sb.append("    idleTimeout: ").append(toIndentedString(idleTimeout)).append("\n");
+		sb.append("    additionalProperties: ")
+				.append(toIndentedString(additionalProperties))
+				.append("\n");
 		sb.append("}");
 		return sb.toString();
 	}
@@ -128,17 +177,6 @@ public class OptionsTCP {
 								OptionsTCP.openapiRequiredFields.toString()));
 			}
 		}
-
-		Set<Map.Entry<String, JsonElement>> entries = jsonElement.getAsJsonObject().entrySet();
-		// check to see if the JSON string contains additional fields
-		for (Map.Entry<String, JsonElement> entry : entries) {
-			if (!OptionsTCP.openapiFields.contains(entry.getKey())) {
-				throw new IllegalArgumentException(
-						String.format(
-								"The field `%s` in the JSON string is not defined in the `OptionsTCP` properties. JSON: %s",
-								entry.getKey(), jsonElement.toString()));
-			}
-		}
 		JsonObject jsonObj = jsonElement.getAsJsonObject();
 		if ((jsonObj.get("idleTimeout") != null && !jsonObj.get("idleTimeout").isJsonNull())
 				&& !jsonObj.get("idleTimeout").isJsonPrimitive()) {
@@ -165,6 +203,30 @@ public class OptionsTCP {
 						@Override
 						public void write(JsonWriter out, OptionsTCP value) throws IOException {
 							JsonObject obj = thisAdapter.toJsonTree(value).getAsJsonObject();
+							obj.remove("additionalProperties");
+							// serialize additional properties
+							if (value.getAdditionalProperties() != null) {
+								for (Map.Entry<String, Object> entry :
+										value.getAdditionalProperties().entrySet()) {
+									if (entry.getValue() instanceof String)
+										obj.addProperty(entry.getKey(), (String) entry.getValue());
+									else if (entry.getValue() instanceof Number)
+										obj.addProperty(entry.getKey(), (Number) entry.getValue());
+									else if (entry.getValue() instanceof Boolean)
+										obj.addProperty(entry.getKey(), (Boolean) entry.getValue());
+									else if (entry.getValue() instanceof Character)
+										obj.addProperty(
+												entry.getKey(), (Character) entry.getValue());
+									else {
+										JsonElement jsonElement = gson.toJsonTree(entry.getValue());
+										if (jsonElement.isJsonArray()) {
+											obj.add(entry.getKey(), jsonElement.getAsJsonArray());
+										} else {
+											obj.add(entry.getKey(), jsonElement.getAsJsonObject());
+										}
+									}
+								}
+							}
 							elementAdapter.write(out, obj);
 						}
 
@@ -172,7 +234,40 @@ public class OptionsTCP {
 						public OptionsTCP read(JsonReader in) throws IOException {
 							JsonElement jsonElement = elementAdapter.read(in);
 							validateJsonElement(jsonElement);
-							return thisAdapter.fromJsonTree(jsonElement);
+							JsonObject jsonObj = jsonElement.getAsJsonObject();
+							// store additional fields in the deserialized instance
+							OptionsTCP instance = thisAdapter.fromJsonTree(jsonObj);
+							for (Map.Entry<String, JsonElement> entry : jsonObj.entrySet()) {
+								if (!openapiFields.contains(entry.getKey())) {
+									if (entry.getValue().isJsonPrimitive()) { // primitive type
+										if (entry.getValue().getAsJsonPrimitive().isString())
+											instance.putAdditionalProperty(
+													entry.getKey(), entry.getValue().getAsString());
+										else if (entry.getValue().getAsJsonPrimitive().isNumber())
+											instance.putAdditionalProperty(
+													entry.getKey(), entry.getValue().getAsNumber());
+										else if (entry.getValue().getAsJsonPrimitive().isBoolean())
+											instance.putAdditionalProperty(
+													entry.getKey(),
+													entry.getValue().getAsBoolean());
+										else
+											throw new IllegalArgumentException(
+													String.format(
+															"The field `%s` has unknown primitive type. Value: %s",
+															entry.getKey(),
+															entry.getValue().toString()));
+									} else if (entry.getValue().isJsonArray()) {
+										instance.putAdditionalProperty(
+												entry.getKey(),
+												gson.fromJson(entry.getValue(), List.class));
+									} else { // JSON object
+										instance.putAdditionalProperty(
+												entry.getKey(),
+												gson.fromJson(entry.getValue(), HashMap.class));
+									}
+								}
+							}
+							return instance;
 						}
 					}.nullSafe();
 		}
--- services/loadbalancer/src/main/java/cloud/stackit/sdk/loadbalancer/model/OptionsUDP.java
+++ services/loadbalancer/src/main/java/cloud/stackit/sdk/loadbalancer/model/OptionsUDP.java
@@ -24,10 +24,11 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 
 /** ProtocolOptionsUDP options to be configured for the PROTOCOL_UDP protocol. */
 @javax.annotation.Generated(
@@ -60,6 +61,50 @@ public class OptionsUDP {
 		this.idleTimeout = idleTimeout;
 	}
 
+	/**
+	 * A container for additional, undeclared properties. This is a holder for any undeclared
+	 * properties as specified with the 'additionalProperties' keyword in the OAS document.
+	 */
+	private Map<String, Object> additionalProperties;
+
+	/**
+	 * Set the additional (undeclared) property with the specified name and value. If the property
+	 * does not already exist, create it otherwise replace it.
+	 *
+	 * @param key name of the property
+	 * @param value value of the property
+	 * @return the OptionsUDP instance itself
+	 */
+	public OptionsUDP putAdditionalProperty(String key, Object value) {
+		if (this.additionalProperties == null) {
+			this.additionalProperties = new HashMap<String, Object>();
+		}
+		this.additionalProperties.put(key, value);
+		return this;
+	}
+
+	/**
+	 * Return the additional (undeclared) property.
+	 *
+	 * @return a map of objects
+	 */
+	public Map<String, Object> getAdditionalProperties() {
+		return additionalProperties;
+	}
+
+	/**
+	 * Return the additional (undeclared) property with the specified name.
+	 *
+	 * @param key name of the property
+	 * @return an object
+	 */
+	public Object getAdditionalProperty(String key) {
+		if (this.additionalProperties == null) {
+			return null;
+		}
+		return this.additionalProperties.get(key);
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) {
@@ -69,12 +114,13 @@ public class OptionsUDP {
 			return false;
 		}
 		OptionsUDP optionsUDP = (OptionsUDP) o;
-		return Objects.equals(this.idleTimeout, optionsUDP.idleTimeout);
+		return Objects.equals(this.idleTimeout, optionsUDP.idleTimeout)
+				&& Objects.equals(this.additionalProperties, optionsUDP.additionalProperties);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(idleTimeout);
+		return Objects.hash(idleTimeout, additionalProperties);
 	}
 
 	@Override
@@ -82,6 +128,9 @@ public class OptionsUDP {
 		StringBuilder sb = new StringBuilder();
 		sb.append("class OptionsUDP {\n");
 		sb.append("    idleTimeout: ").append(toIndentedString(idleTimeout)).append("\n");
+		sb.append("    additionalProperties: ")
+				.append(toIndentedString(additionalProperties))
+				.append("\n");
 		sb.append("}");
 		return sb.toString();
 	}
@@ -124,17 +173,6 @@ public class OptionsUDP {
 								OptionsUDP.openapiRequiredFields.toString()));
 			}
 		}
-
-		Set<Map.Entry<String, JsonElement>> entries = jsonElement.getAsJsonObject().entrySet();
-		// check to see if the JSON string contains additional fields
-		for (Map.Entry<String, JsonElement> entry : entries) {
-			if (!OptionsUDP.openapiFields.contains(entry.getKey())) {
-				throw new IllegalArgumentException(
-						String.format(
-								"The field `%s` in the JSON string is not defined in the `OptionsUDP` properties. JSON: %s",
-								entry.getKey(), jsonElement.toString()));
-			}
-		}
 		JsonObject jsonObj = jsonElement.getAsJsonObject();
 		if ((jsonObj.get("idleTimeout") != null && !jsonObj.get("idleTimeout").isJsonNull())
 				&& !jsonObj.get("idleTimeout").isJsonPrimitive()) {
@@ -161,6 +199,30 @@ public class OptionsUDP {
 						@Override
 						public void write(JsonWriter out, OptionsUDP value) throws IOException {
 							JsonObject obj = thisAdapter.toJsonTree(value).getAsJsonObject();
+							obj.remove("additionalProperties");
+							// serialize additional properties
+							if (value.getAdditionalProperties() != null) {
+								for (Map.Entry<String, Object> entry :
+										value.getAdditionalProperties().entrySet()) {
+									if (entry.getValue() instanceof String)
+										obj.addProperty(entry.getKey(), (String) entry.getValue());
+									else if (entry.getValue() instanceof Number)
+										obj.addProperty(entry.getKey(), (Number) entry.getValue());
+									else if (entry.getValue() instanceof Boolean)
+										obj.addProperty(entry.getKey(), (Boolean) entry.getValue());
+									else if (entry.getValue() instanceof Character)
+										obj.addProperty(
+												entry.getKey(), (Character) entry.getValue());
+									else {
+										JsonElement jsonElement = gson.toJsonTree(entry.getValue());
+										if (jsonElement.isJsonArray()) {
+											obj.add(entry.getKey(), jsonElement.getAsJsonArray());
+										} else {
+											obj.add(entry.getKey(), jsonElement.getAsJsonObject());
+										}
+									}
+								}
+							}
 							elementAdapter.write(out, obj);
 						}
 
@@ -168,7 +230,40 @@ public class OptionsUDP {
 						public OptionsUDP read(JsonReader in) throws IOException {
 							JsonElement jsonElement = elementAdapter.read(in);
 							validateJsonElement(jsonElement);
-							return thisAdapter.fromJsonTree(jsonElement);
+							JsonObject jsonObj = jsonElement.getAsJsonObject();
+							// store additional fields in the deserialized instance
+							OptionsUDP instance = thisAdapter.fromJsonTree(jsonObj);
+							for (Map.Entry<String, JsonElement> entry : jsonObj.entrySet()) {
+								if (!openapiFields.contains(entry.getKey())) {
+									if (entry.getValue().isJsonPrimitive()) { // primitive type
+										if (entry.getValue().getAsJsonPrimitive().isString())
+											instance.putAdditionalProperty(
+													entry.getKey(), entry.getValue().getAsString());
+										else if (entry.getValue().getAsJsonPrimitive().isNumber())
+											instance.putAdditionalProperty(
+													entry.getKey(), entry.getValue().getAsNumber());
+										else if (entry.getValue().getAsJsonPrimitive().isBoolean())
+											instance.putAdditionalProperty(
+													entry.getKey(),
+													entry.getValue().getAsBoolean());
+										else
+											throw new IllegalArgumentException(
+													String.format(
+															"The field `%s` has unknown primitive type. Value: %s",
+															entry.getKey(),
+															entry.getValue().toString()));
+									} else if (entry.getValue().isJsonArray()) {
+										instance.putAdditionalProperty(
+												entry.getKey(),
+												gson.fromJson(entry.getValue(), List.class));
+									} else { // JSON object
+										instance.putAdditionalProperty(
+												entry.getKey(),
+												gson.fromJson(entry.getValue(), HashMap.class));
+									}
+								}
+							}
+							return instance;
 						}
 					}.nullSafe();
 		}
