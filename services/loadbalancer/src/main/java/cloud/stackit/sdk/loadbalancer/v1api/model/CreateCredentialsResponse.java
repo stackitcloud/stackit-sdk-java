168deaca88899ee39ae4c6321760bcb00be86759
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2026-07-28 12:46:15 +0200

    Generate loadbalancer (#782)

--- services/loadbalancer/src/main/java/cloud/stackit/sdk/loadbalancer/v1api/model/CreateCredentialsResponse.java
+++ services/loadbalancer/src/main/java/cloud/stackit/sdk/loadbalancer/v1api/model/CreateCredentialsResponse.java
@@ -31,9 +31,7 @@ import java.util.Map;
 import java.util.Objects;
 
 /** CreateCredentialsResponse */
-@javax.annotation.Generated(
-		value = "org.openapitools.codegen.languages.JavaClientCodegen",
-		comments = "Generator version: 7.19.0")
+@javax.annotation.Generated(value = "JavaGenerator", comments = "Generator version: 7.19.0")
 public class CreateCredentialsResponse {
 	public static final String SERIALIZED_NAME_CREDENTIAL = "credential";
 
--- services/loadbalancer/src/main/java/cloud/stackit/sdk/loadbalancer/v1api/model/CreateLoadBalancerPayload.java
+++ services/loadbalancer/src/main/java/cloud/stackit/sdk/loadbalancer/v1api/model/CreateLoadBalancerPayload.java
@@ -34,9 +34,7 @@ import java.util.Map;
 import java.util.Objects;
 
 /** CreateLoadBalancerPayload */
-@javax.annotation.Generated(
-		value = "org.openapitools.codegen.languages.JavaClientCodegen",
-		comments = "Generator version: 7.19.0")
+@javax.annotation.Generated(value = "JavaGenerator", comments = "Generator version: 7.19.0")
 public class CreateLoadBalancerPayload {
 	public static final String SERIALIZED_NAME_ERRORS = "errors";
 
