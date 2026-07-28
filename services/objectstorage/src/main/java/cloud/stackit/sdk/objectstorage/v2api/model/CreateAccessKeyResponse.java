d3d60403e837059aac0a61d66085842f14dda9d2
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2026-07-28 11:55:26 +0200

    Generate objectstorage (#776)
    Co-authored-by: cgoetz-inovex <carlo.goetz@inovex.de>

--- services/objectstorage/src/main/java/cloud/stackit/sdk/objectstorage/v2api/model/CreateAccessKeyResponse.java
+++ services/objectstorage/src/main/java/cloud/stackit/sdk/objectstorage/v2api/model/CreateAccessKeyResponse.java
@@ -31,9 +31,7 @@ import java.util.Map;
 import java.util.Objects;
 
 /** CreateAccessKeyResponse */
-@javax.annotation.Generated(
-		value = "org.openapitools.codegen.languages.JavaClientCodegen",
-		comments = "Generator version: 7.19.0")
+@javax.annotation.Generated(value = "JavaGenerator", comments = "Generator version: 7.19.0")
 public class CreateAccessKeyResponse {
 	public static final String SERIALIZED_NAME_ACCESS_KEY = "accessKey";
 
--- services/objectstorage/src/main/java/cloud/stackit/sdk/objectstorage/v2api/model/GetCredentialsGroupResponse.java
+++ services/objectstorage/src/main/java/cloud/stackit/sdk/objectstorage/v2api/model/GetCredentialsGroupResponse.java
@@ -31,9 +31,7 @@ import java.util.Map;
 import java.util.Objects;
 
 /** GetCredentialsGroupResponse */
-@javax.annotation.Generated(
-		value = "org.openapitools.codegen.languages.JavaClientCodegen",
-		comments = "Generator version: 7.19.0")
+@javax.annotation.Generated(value = "JavaGenerator", comments = "Generator version: 7.19.0")
 public class GetCredentialsGroupResponse {
 	public static final String SERIALIZED_NAME_CREDENTIALS_GROUP = "credentialsGroup";
 
